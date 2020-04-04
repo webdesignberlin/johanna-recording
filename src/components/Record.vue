eea3451180bbb1f1bcd3a4a5730459a4b3b2d285
Michael Gerstmann <michael.gerstmann@fitx.de>
2020-04-04 20:32:01 +0200

    test polyfill

--- src/components/Record.vue
+++ src/components/Record.vue
@@ -12,7 +12,7 @@
       class="play"
       :class="{ active: playing }"
       :src="recordedAudio.src"
-      :controls="recordedAudio.controls"
+      :controls="true"
       :autoplay="true"
       @click="play"
       ref="player"
@@ -32,7 +32,9 @@ export default {
     return {
       rec: null,
       audioChunks: [],
-      recordedAudio: {},
+      recordedAudio: {
+        src: null,
+      },
       recording: false,
       playing: false,
     };
@@ -45,38 +47,29 @@ export default {
   },
   methods: {
     init() {
-      // polyfill
       if (!window.MediaRecorder) {
         window.MediaRecorder = AudioRecorder;
       }
-      navigator.mediaDevices.getUserMedia({ audio: true })
-        .then((stream) => { this.handlerFunction(stream); });
     },
-    handlerFunction(stream) {
-      this.rec = new MediaRecorder(stream);
-      this.rec.ondataavailable = (e) => {
-        this.audioChunks.push(e.data);
-        if (this.rec.state === 'inactive') {
-          const blob = new Blob(this.audioChunks,{ // type: 'audio/mpeg-3'
-          });
-          this.recordedAudio = {
-            src: URL.createObjectURL(blob),
-            controls: true,
-            autoplay: true,
-          };
-          this.sendData(blob);
-        }
-      };
-    },
-    sendData() {},
-    record(e) {
-      this.recording = !this.recording;
-
+    record() {
       if (this.recording) {
-        this.rec.start(e);
-      } else {
         this.rec.stop();
+        this.rec.stream.getTracks().forEach((i) => i.stop());
+      } else {
+        navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+          this.rec = new MediaRecorder(stream);
+
+          // Set record to <audio> when recording will be finished
+          this.rec.addEventListener('dataavailable', (ev) => {
+            console.log('src', ev);
+            this.recordedAudio.src = URL.createObjectURL(ev.data);
+          });
+
+          // Start recording
+          this.rec.start();
+        });
       }
+      this.recording = !this.recording;
     },
     play() {
       this.$refs.player.play();
@@ -116,9 +109,13 @@ export default {
   cursor: pointer;
   box-shadow: 0 0 0 0 rgba(232, 76, 61, 0.7);
 
+  &::-webkit-media-controls,
   &::-webkit-media-controls-panel,
   &::-webkit-media-controls-enclosure {
-    display: none;
+    display: none !important;
+    width: 1px;
+    height: 1px;
+    opacity: 0;
   }
 
   &.active {
